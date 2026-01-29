4627dc50c284d42e1d0c3dc35ef4e72ae1c28abb
jhdvrd <juho.karvonen@outlook.com>
2026-01-29 14:39:18 +0200

    first commit

---
+++ api/data.js
@@ -0,0 +1,55 @@
+import { Router } from 'express';
+import {getAllData, getDataById, addData, updateData, deleteData} from '../db/db.js';
+let router = Router()
+
+router.get('/', async (req, res) => {
+    res.json( await getAllData() )
+})
+
+router.get('/:id', async (req, res) => {
+    res.json( await getDataById(req.params.id) )
+})
+
+router.post('/', async (req, res) => {
+    let [exist] = await getDataById(req.body.id)
+    if( exist ) {
+        res.status(409).json( {"error": "record already exists"});
+    } else {
+        let result = await addData(req.body);
+        if(result)
+            res.json(req.body);
+        else
+            res.status(500).json({"error": "unknown database error"})
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    let [exist] = await getDataById(req.params.id);
+    if (!exist) {
+        res.status(404).json({ "error": "record not found" });
+    } else {
+        let result = await updateData(req.params.id, req.body);
+        if (result) {
+            res.json(req.body);
+        } else {
+            res.status(500).json({ "error": "unknown database error" });
+        }
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    let [exist] = await getDataById(req.params.id);
+    if (!exist) {
+        res.status(404).json({ "error": "record not found" });
+    } else {
+        let result = await deleteData(req.params.id);
+        if (result) {
+            res.status(204).send();
+        } else {
+            res.status(500).json({ "error": "unknown database error" });
+        }
+    }
+});
+
+export default router;
+
